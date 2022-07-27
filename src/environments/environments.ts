6f8b9c3922fd21197ad40633d9b68f08f0eb4ac4
VijayAnand <vijayanandof@users.noreply.github.com>
2022-07-27 14:22:01 +0530

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'AIzaSyCNtXbDYVBs--gprMft0Aeep_Ox0X4j2Ic',
-        authDomain: 'foodlogic-31e18.firebaseapp.com',
-        databaseURL: 'https://foodlogic-31e18.firebaseio.com',
-        projectId: 'foodlogic-31e18',
-        storageBucket: 'foodlogic-31e18.appspot.com',
-        messagingSenderId: '884922975798'
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: ''
     }
   };
